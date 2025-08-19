d15fc2df9c511f879be55be32507a393663bb8cb
Mike Girenko <mike.girenko@processunity.com>
2025-08-19 07:56:02 -0600

    Added logger and pytest.ini

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,12 +1,20 @@
+import pytest
 import re
+import logging
 from playwright.sync_api import Page, expect
 
 from pages.home_page import navigate_to_me
 
+logger = logging.getLogger(__name__)
 
+
+@pytest.mark.all_tests
 def test_home_page_has_title(page: Page) -> None:
+    logger.info("Starting test to check Home page title")
     navigate_to_me(page)
     page.screenshot(path="reports/screenshots/home.png")
 
     # Check page title
     expect(page).to_have_title(re.compile("Software Quality Assurance & Testing"))
+
+    logger.info("Ending test to check Home page title")
--- tests/test_questions_page.py
+++ tests/test_questions_page.py
@@ -1,24 +1,28 @@
-"""
---Install the Pytest plugin: pip install pytest-playwright
---Install the required browsers: playwright install
-I do not need to do this because I have pyenv, and it already has the above
-"""
-
+import pytest
 import re
+import logging
 from playwright.sync_api import Page, expect
 
-from common.common import BASE_URL
 from pages.questions_page import navigate_to_me
 
+logger = logging.getLogger(__name__)
 
+
+@pytest.mark.all_tests
 def test_questions_page_has_title(page: Page) -> None:
+    logger.info("Starting test to check Questions page title")
     navigate_to_me(page)
     page.screenshot(path="reports/screenshots/newest_questions.png")
 
     # Check page title
     expect(page).to_have_title(re.compile("Newest Questions"))
 
+    logger.info("Ending test to check Questions page title")
+
+
+@pytest.mark.all_tests
 def test_ask_question_button(page: Page) -> None:
+    logger.info("Starting test to check Ask Question button")
     navigate_to_me(page)
 
     # Click the Ask Question button.
@@ -27,3 +31,6 @@ def test_ask_question_button(page: Page) -> None:
     # Check page "Ask a public question" text.
     expect(page.get_by_text("Ask a public question")).to_be_visible()
     #  page.wait_for_timeout(5000)
+
+    logger.info("Ending test to check Questions page title")
+
